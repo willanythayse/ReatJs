fe7efbf9c8dfaa01626dd9a5823c10833d4f4710
Willany Thayse <willany.thayse@gmail.com>
2018-04-18 01:08:05 -0300

    Inicio app e public

---
+++ app/App.js
@@ -0,0 +1,4 @@
+var React = require('react'); // importa a lib react
+var reactDOM = require('react-dom'); // importa a lib react-dom
+
+reactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
